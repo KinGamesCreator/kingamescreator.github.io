210b8fd559f6b72a9181e30adff2a6c8dd0ada60
KinGamesCreator <kingames.cuenta@gmail.com>
2022-11-03 20:13:37 -0300

    expo

---
+++ EXPO/players.js
@@ -0,0 +1,23 @@
+var host = '20.40.51.233:1235';
+var socket;
+
+
+connection = false;
+
+function setup() {
+  createCanvas(400, 400);
+
+  socket = new WebSocket('ws://' + host);
+  socket.onopen = () => connection = true;
+  socket.onclose = () => connection = false;
+}
+
+function draw() {
+  background("#2307AF");
+  if (!mouseIsPressed) background("#FF0000"); //else fullscreen(true);
+  if (connection) socket.send(JSON.stringify({status:mouseIsPressed}));
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth,windowHeight);
+}
